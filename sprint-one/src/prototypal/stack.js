509074bb4acf95b85f465e9ded4c5322cd1f8337
leadpython <jeffrey.campecino@gmail.com>
2015-10-07 15:19:33 -0700

    Completed stack.js for prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = Object.create(stackMethods);
+  storage.length = 0;
+  return storage;
 };
 
-var stackMethods = {};
+
+
+var stackMethods = {
+  push: function(value) {
+    this[this.length] = value;
+    this.length++;
+  },
+  pop: function() {
+    if(this.length > 0){
+      this.length--;
+    }
+    var result = this[this.length];
+    delete this[this.length];
+    return result;
+  },
+  size: function() {
+    return this.length;
+  }
+};
 
 
