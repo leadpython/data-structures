6b9da14172472dfcb19a75273c9687aeb2e068c2
leadpython <jeffrey.campecino@gmail.com>
2015-10-07 14:56:25 -0700

    Completed stack.js functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,44 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage.length = 0;
+  //Stack.methods = stackMethods;
+  extend(storage, stackMethods);
+
+
+  
+
+  return storage;
 };
 
-var stackMethods = {};
+var extend = function(to, from)
+  {
+    for(var key in from)
+    {
+      to[key] = from[key];
+    }
+};
+
+var stackMethods = {
+
+  pop: function(){
+    if (this.length > 0) {
+      this.length--;
+    }
+    var result = this[this.length];
+    delete this[this.length];
+    return result;
+  },
+  push: function(value){
+    this[this.length] = value;
+    this.length++;
+
+  },
+  size: function(){
+    return this.length;
+  }
+
+};
 
 
