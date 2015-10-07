c533acaecd5b6d57580050b409b6833c31477576
leadpython <jeffrey.campecino@gmail.com>
2015-10-07 15:52:55 -0700

    Completed queue.js (pseudoclassical)

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
 };
+
+Queue.prototype.enqueue = function(value) {
+  this[this.length] = value;
+  this.length++;
+};
+Queue.prototype.dequeue = function() {
+  if (this.length > 0) {
+    this.length--;
+  }
+  var result = this[0];
+  delete this[0];
+
+  for (var i = 0; i < this.size(); i++) {
+    this[i] = this[i+1];
+    delete this[i+1];
+  }
+
+  return result;
+};
+Queue.prototype.size = function() {
+  return this.length;
+}
 
 
