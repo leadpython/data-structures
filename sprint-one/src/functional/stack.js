cdee8bf8378adb1bfff47d4621f8469277e83856
Jeffrey Campecino <jeffrey.campecino@gmail.com>
2015-10-07 12:09:06 -0700

    stack is fully functional

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var key = length;
+    storage[key] = value;
+    length++;
   };
 
   someInstance.pop = function() {
+    if (length > 0) {
+      length--;
+    }
+    var key = length;
+    var result = storage[key];
+    delete storage[key];
+    return result;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
