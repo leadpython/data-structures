5c32d5efbc33c5a05c731a040daaa4380092fc93
leadpython <jeffrey.campecino@gmail.com>
2015-10-08 15:21:05 -0700

    Completed tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,19 +3,47 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+
+  extend(newTree, treeMethods);
 
   return newTree;
+};
+
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   // your code here
-  newTree.children = null;  // fix me
+  
+  //var node = Object.create(Tree);
+  //this.children.push()
+  this.children.push(Tree(value));
+
+  
 };
 
 treeMethods.contains = function(target) {
+  var doesItContain = false;
+
+  var checkTarget = function(nodeTree) {
+    if (nodeTree.value === target) {
+      doesItContain = true;
+    } else {
+      for (var i = 0; i < nodeTree.children.length; i++) {
+        checkTarget(nodeTree.children[i]);
+      }
+    }
+  };
+  checkTarget(this);
+  return doesItContain;
 };
 
 
