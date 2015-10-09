13ba54f4655ced3d98904ae801330084faa4e8cb
leadpython <jeffrey.campecino@gmail.com>
2015-10-08 18:02:10 -0700

    completed graph.js

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,41 +3,75 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+  
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+
+  //create node from the node value
+  var graphNode = {};
+  this[node] = graphNode;
+
+
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  var doesItContain = false;
+  for (var key in this) {
+    if (Number(key) === node){
+      doesItContain = true;
+    }
+  }
+  return doesItContain;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var key in this) {
+    if (Number(key) === node) {
+      delete this[key];
+    }
+  }
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  var doesItContain = false;
+  for(var key in this[fromNode]){
+    if(Number(key) === toNode){
+      doesItContain = true;
+    }
+  }
+  return doesItContain;
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this[fromNode][toNode] = this[toNode];
+  this[toNode][fromNode] = this[fromNode];
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this[fromNode][toNode];
+  delete this[toNode][fromNode];
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for(var key in this){
+
+    cb(key);
+  }
 };
 
 /*
