5312be88bf92fda202745426d589a860859738af
leadpython <jeffrey.campecino@gmail.com>
2015-10-08 18:20:58 -0700

    set.js completed

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,38 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  var doesItContain = false;
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      doesItContain = true;
+    }
+  }
+  return doesItContain;
 };
 
 setPrototype.remove = function(item) {
+  var target = 0;
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      target = i;
+    }
+  }
+  for (var i = target; i < this._storage.length; i++) {
+    this._storage[i] = this._storage[i+1];
+  }
+  this._storage.pop();
 };
 
 /*
